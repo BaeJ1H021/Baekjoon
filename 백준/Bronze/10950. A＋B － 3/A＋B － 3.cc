4b4a7b1859ee2eae466fb954be4e59c9b2496d84
배지호 <87417773+BaeJ1H021@users.noreply.github.com>
2024-12-26 19:18:38 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  int a,b;
+  for (int i=0;i<n;i++)
+  {
+    cin >> a >> b;
+    cout << a+b << endl;
+  }
+	return 0;
+}
